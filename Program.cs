d67fb93018f4f60939f38ab90f8aab96f697e497
zhufucdev <1095291434@qq.com>
2022-03-17 21:16:53 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace EdgeRemover
+{
+    internal class Program
+    {
+        private const string EDGE_PROTOCOL = "microsoft-edge:?";
+        private const string IFEO_PATH = @"SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\msedge.exe";
+        private const string CONFIGURATION_NAME = "desired_broswer.txt";
+
+        private static string ConfigurationPath
+        {
+            get => Path.Combine(Assembly.GetExecutingAssembly().Location, CONFIGURATION_NAME);
+        }
+
+        static void Main(string[] args)
+        {
+            var query = args.FirstOrDefault(v => v.StartsWith(EDGE_PROTOCOL));
+            if (query == null)
+            {
+                if (!IsUserAdmin())
+                {
+                    RelaunchAsAdmin();
+                }
+                Register();
+            }
+            else
+            {
+                var index = query.IndexOf("url=");
+                if (index < 0)
+                {
+                    Console.WriteLine(string.Format("Unexpected input: {0}", query));
+                    PreExit(1);
+                }
+
+                var indexEnd = query.IndexOf('&', index + 1);
+                if (indexEnd < 0)
+                {
+                    indexEnd = query.Length;
+                }
+                var url = HttpUtility.UrlDecode(query.Substring(index + 4, indexEnd - index - 4));
+                if (File.Exists(ConfigurationPath))
+                {
+                    var desired = File.ReadAllText(ConfigurationPath);
+                    var process = new ProcessStartInfo()
+                    {
+                        FileName = ConfigurationPath
+                    };
+                    Process.Start(process);
+                } 
+                else
+                {
+                    Console.WriteLine("Error: No defined broswer found.");
+                    Console.WriteLine(string.Format("Write plain text of the path to your broswer executable in {0}", ConfigurationPath));
+                    PreExit();
+                }
+            }
+        }
+
+        static bool IsUserAdmin()
+        {
+            bool isAdmin = false;
+            try
+            {
+                WindowsIdentity user = WindowsIdentity.GetCurrent();
+                WindowsPrincipal principal = new WindowsPrincipal(user);
+                isAdmin = principal.IsInRole(WindowsBuiltInRole.Administrator);
+            }
+            catch (Exception)
+            {
+            }
+            return isAdmin;
+        }
+
+        static void RelaunchAsAdmin()
+        {
+            Console.WriteLine("This app is invoked by user acively.");
+            Console.WriteLine("==> Will register itself in Image File Execution Options registry.");
+            ProcessStartInfo proc = new ProcessStartInfo
+            {
+                UseShellExecute = true,
+                WorkingDirectory = Environment.CurrentDirectory,
+                FileName = Assembly.GetEntryAssembly().CodeBase,
+                Verb = "runas"
+            };
+            try
+            {
+                Process.Start(proc);
+                Environment.Exit(0);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: This program must be run as an administrator! \n\n" + ex.ToString());
+                PreExit(1);
+            }
+        }
+
+        static void Register()
+        {
+            try
+            {
+                using (RegistryKey key = Registry.LocalMachine.CreateSubKey(IFEO_PATH))
+                {
+                    key.SetValue("Debug", Assembly.GetExecutingAssembly().Location);
+                }
+            } catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            Console.WriteLine("Registration successful.");
+            Console.WriteLine(string.Format(@"See Computer\HKEY_LOCAL_MACHINE\{0} for details.", IFEO_PATH));
+            PreExit();
+        }
+
+        static void PreExit(int State = 0)
+        {
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+            Environment.Exit(State);
+        }
+    }
+}
