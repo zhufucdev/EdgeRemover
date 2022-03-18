a89f9a5f27666b57250b9507cb9cdc2cb50f46d1
ZhuFu <1095291434@qq.com>
2022-03-18 13:06:11 +0800

    Use MessageBox instead Improved accessibility

--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Security.Principal;
 using System.Web;
+using System.Windows.Forms;
 
 namespace EdgeRemover
 {
@@ -12,11 +14,13 @@ namespace EdgeRemover
     {
         private const string EDGE_PROTOCOL = "microsoft-edge:?";
         private const string IFEO_PATH = @"SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\msedge.exe";
+        private const string EDGE_EXE = "msedge.exe";
+        private const string ER_FLAG = "--edge-remover";
+        private static readonly string[] IGNORED_FLAGS = new string[] { "--single-argument" };
 
         static void Main(string[] args)
         {
-            var query = args.FirstOrDefault(v => v.StartsWith(EDGE_PROTOCOL));
-            if (query == null)
+            if (args.Length == 0)
             {
                 if (!IsUserAdmin())
                 {
@@ -26,25 +30,85 @@ namespace EdgeRemover
             }
             else
             {
-                var index = query.IndexOf("url=");
-                if (index < 0)
-                {
-                    Console.WriteLine(string.Format("Unexpected input: {0}", query));
-                    PreExit(1);
-                }
+                string url, extra;
 
-                var indexEnd = query.IndexOf('&', index + 1);
-                if (indexEnd < 0)
+                var query = args.FirstOrDefault(v => v.StartsWith(EDGE_PROTOCOL));
+                if (query != null)
                 {
-                    indexEnd = query.Length;
+                    var index = query.IndexOf("url=");
+                    if (index < 0)
+                    {
+                        MessageBox.Show(string.Format("Unexpected input: {0}", query), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        Environment.Exit(1);
+                    }
+
+                    var indexEnd = query.IndexOf('&', index + 1);
+                    if (indexEnd < 0)
+                    {
+                        indexEnd = query.Length;
+                    }
+                    url = HttpUtility.UrlDecode(query.Substring(index + 4, indexEnd - index - 4));
+                    extra = "";
                 }
-                var url = HttpUtility.UrlDecode(query.Substring(index + 4, indexEnd - index - 4));
-                var process = new ProcessStartInfo()
+                else if (args[0].Contains(EDGE_EXE))
                 {
-                    FileName = url
-                };
-                Process.Start(process);
+                    url = string.Empty;
+                    extra = string.Empty;
+                    if (args.Count() <= 1)
+                    {
+                        // this is a normal call
+                        MessageBox.Show("Microsoft Edge won't be called in your system.", "EdgeRemover", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        Environment.Exit(1);
+                    }
+                    else if (args.Contains(ER_FLAG) || DateTime.Now - Settings.Default.LastCall < TimeSpan.FromSeconds(1))
+                    {
+                        // this is a loopback
+                        MessageBox.Show("This file is set to be opened with Edge, which is blocked.\nTry another app.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        Environment.Exit(1);
+                    }
+                    else
+                    {
+                        for (var i = 1; i < args.Length; i++)
+                        {
+                            if (IGNORED_FLAGS.Contains(args[i]))
+                            {
+                                continue;
+                            }
+                            if (url == string.Empty)
+                            {
+                                url = args[i];
+                            }
+                            else
+                            {
+                                extra += args[i] + " ";
+                            }
+                        }
+                    }
+                } 
+                else
+                {
+                    extra = string.Empty;
+                    url = args[0];
+                    for (var i = 1; i < args.Length; i++)
+                    {
+                        extra += args[i] + " ";
+                    }
+                }
+                Open(url, extra);
             }
+        }
+
+        static void Open(string file, string extraArg)
+        {
+            Settings.Default.LastCall = DateTime.Now;
+            Settings.Default.Save();
+
+            var process = new ProcessStartInfo()
+            {
+                FileName = file,
+                Arguments = extraArg
+            };
+            Process.Start(process);
         }
 
         static bool IsUserAdmin()
@@ -78,10 +142,14 @@ namespace EdgeRemover
                 Process.Start(proc);
                 Environment.Exit(0);
             }
-            catch (Exception ex)
+            catch (Exception)
             {
-                Console.WriteLine("Error: This program must be run as an administrator! \n\n" + ex.ToString());
-                PreExit(1);
+                MessageBox.Show(
+                    "This program must be run as an administrator this time!",
+                    "Registration failed",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error
+                );
             }
         }
 
@@ -93,22 +161,14 @@ namespace EdgeRemover
                 {
                     key.SetValue("Debugger", Assembly.GetExecutingAssembly().Location);
                 }
-            } 
+            }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.ToString());
-                PreExit(1);
+                Environment.Exit(1);
             }
-            Console.WriteLine("Registration successful.");
-            Console.WriteLine(string.Format(@"See Computer\HKEY_LOCAL_MACHINE\{0} for details.", IFEO_PATH));
-            PreExit();
+            MessageBox.Show(string.Format(@"See Computer\HKEY_LOCAL_MACHINE\{0} for details.", IFEO_PATH), "Registration successful");
         }
 
-        static void PreExit(int State = 0)
-        {
-            Console.WriteLine("Press any key to exit.");
-            Console.ReadKey();
-            Environment.Exit(State);
-        }
     }
 }
